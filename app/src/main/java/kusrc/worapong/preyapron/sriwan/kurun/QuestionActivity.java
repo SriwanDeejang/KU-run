0d5d3b57c24d5e500236c218737238965f2db4ac
Sriwan Deejang <noowhan_whan@outlook.co.th>
2016-05-07 11:59:24 +0700

    แยกการทำงาน

--- app/src/main/java/kusrc/worapong/preyapron/sriwan/kurun/QuestionActivity.java
+++ app/src/main/java/kusrc/worapong/preyapron/sriwan/kurun/QuestionActivity.java
@@ -2,6 +2,7 @@ package kusrc.worapong.preyapron.sriwan.kurun;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class QuestionActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class QuestionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question);
+    }   // Main Method
+
+    public void clickAnswer(View view){
+
     }
-}
+
+}   // Main Class
